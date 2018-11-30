819904cf2e6f4925aa7615c0921f3fcbaff44a4e
Ireneusz Pastusiak <ireneusz.pastusiak@gmail.com>
2018-11-30 16:06:26 +0000

    Add branch-specific unit test

---
+++ src/test/java/uk/co/automatictester/lambdatestrunner/YetAnotherSmokeTest.java
@@ -0,0 +1,13 @@
+package uk.co.automatictester.lambdatestrunner;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+public class YetAnotherSmokeTest {
+
+    @Test
+    public void runSmokeTest() {
+        assertTrue(true);
+    }
+}
